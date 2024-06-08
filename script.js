b7ff38d314fd57aa73a42cdbe2cc4437f831d39d
Wayfarerdesert <125356747+Wayfarerdesert@users.noreply.github.com>
2024-06-09 01:04:12 +0200

    Selected movie details render

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 const movieSearchBox = document.getElementById('movie-search-box');
 const searchList = document.getElementById('search-list');
 const resultGrid = document.getElementById('result-grid');
@@ -47,4 +45,49 @@ function displayMovieList(movies) {
         `;
         searchList.appendChild(movieListItem);
     }
+    loadMovieDetails();
 };
+
+function loadMovieDetails() {
+    const searchListMovies = searchList.querySelectorAll('.search-list-item');
+    searchListMovies.forEach(movie => {
+        movie.addEventListener('click', async () => {
+            // console.log(movie.dataset.id);
+            searchList.classList.add('hide-search-list');
+            movieSearchBox.value = "";
+            const result = await fetch(`https://www.omdbapi.com/?apikey=8502611c&i=${movie.dataset.id}`)
+
+            const movieDetails = await result.json();
+            // console.log(movieDetails);
+            displayMovieDetails(movieDetails);
+        });
+    });
+}
+
+function displayMovieDetails(details) {
+    resultGrid.innerHTML = `
+        <div class="movie-poster">
+            <img src="${(details.Poster != 'N/A') ? details.Poster : './assets/no-image.webp'}" alt="movie poster">
+        </div>
+        <div class="movie-info">
+            <h3 class="movie-title">${details.Title}</h3>
+            <ul class="movie-misc-info">
+                <li class="year"><b>Year: </b>${details.Year}</li>
+                <li class="rating">Rating: ${details.Rated}</li>
+                <li class="released">Released: ${details.Released}</li>
+            </ul>
+            <p class="genre"><b>Genre: </b>${details.Genre}</p>
+            <p class="writer"><b>Writer: </b>${details.Writer}</p>
+            <p class="actors"><b>Actors: </b>${details.Actors}</p>
+            <p class="plot"><b>Plot: </b>${details.Plot}</p>
+            <p class="language"><b>Language: </b>${details.Language}</p>
+            <p class="awards"><i class="fas fa-award">&nbsp;</i><b>Awards: </b>${details.Awards}</p>
+        </div>
+    `;
+};
+
+window.addEventListener('click', (event) => {
+    if (event.target.className != 'form-control') {
+        searchList.classList.add('hide-search-list');
+    }
+});
